721fc94c60565c28c2602583658eb1d413e240b1
TecnoNetPC <tecnonetpcsc@gmail.com>
2024-10-28 11:25:10 +0100

    new changes again

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xjp*u0^3*8d!6arjklh@d_j+d1fy*=cb%nlv!zyl9n=jovf5an'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
